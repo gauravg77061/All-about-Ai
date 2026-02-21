c016e3d5d59e9bbe7a25894d2b0e4f5e18316f33
gaurav <gauravg77061@gmail.com>
2026-02-21 13:21:00 +0530

    system restriction

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello from ai")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+# print("KEY =", os.getenv("OPENAI_API_KEY"))
+
+client=OpenAI()
+
+response=client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role":"system","content":"You are an expert in Ai and only and only ans coding related questions. That if query is not related to coding .Just say sorry and don not ans it"},
+        {"role":"user","content":"Hey can solve a maths problem for me deac"}
+    ]
+
+)
+print(response.choices[0].message.content)
